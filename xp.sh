2b2e311432848e5f2c210045c4263e4723d77353
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-12-10 20:44:43 +0800

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -79,32 +79,32 @@ fi
 done
 
 #----- Auto Remove Vless
-data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '^#vls' | cut -d ' ' -f 2 | sort | uniq`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+exp=$(grep -w "^#vls $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" -le "0" ]]; then
-sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vls $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#vlsg $user $exp/,/^},{/d" /etc/xray/config.json
 fi
 done
 
 #----- Auto Remove Trojan
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '^#tr' | cut -d ' ' -f 2 | sort | uniq`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
 do
-exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+exp=$(grep -w "^#tr $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 if [[ "$exp2" -le "0" ]]; then
-sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#tr $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^#trg $user $exp/,/^},{/d" /etc/xray/config.json
 fi
 done
 systemctl restart xray
